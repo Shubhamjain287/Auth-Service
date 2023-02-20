79b3aab6de0a85642a631ac4702eb56ac86f1cd9
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2023-02-20 16:40:05 +0530

    Added USer_Roles and Roles

--- README.md
+++ README.md
@@ -10,3 +10,27 @@
 
 - Migration from development to Database
     `npx sequeize db:migrate`
+
+## Authorization - Using Roles
+
+- Generate New Model for User Roles Like
+    ```npx sequelize model:generate --name Role --attributes name:string```
+
+- For Many to Many Assosiation we have to make a new through table.
+for example :-
+    ```-> User belongs to Many Roles
+    -> Role Belongs to Many USer
+    -> Sequelize Authomatically Generate the Intermidiate Table i.e `User_Roles````
+
+- Database Migration
+    ```npx sequelize db:migrate```
+
+- DB_SYNC for SYNC Database One Time
+
+- Generation Seeding Files For Roles
+    ```npx sequelize seed:generate --name add-roles```
+
+- Seeding Seed File to Database Using
+    ```npx sequelize db:seed --seed 20230220105218-add-roles.js```
+
+- 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,30 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const apiRoutes = require("./routes/index");
-const { PORT } = require("./config/serverConfig");
+const { PORT, DB_SYNC } = require("./config/serverConfig");
+const db = require("./models/index");
 
-const server = () => {
+const server = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     
     app.use("/api",apiRoutes);
 
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(`Server is Runnnig at PORT ${PORT}`);
+
+        if(DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+
+        // const user = await db.User.findByPk(3);
+        // const role = await db.Role.findByPk(1);
+
+        // await user.addRole(role);
+        // const response = await user.hasRole(role);
+        // console.log(response);
+
     });
 
 }
